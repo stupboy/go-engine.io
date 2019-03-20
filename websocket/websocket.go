793dba0b3b970da7edd03dca91653e2f21ca67b9
stupboy_mac <stupboy@mac.com>
2019-03-20 09:51:25 +0800

    init

---
+++ websocket/websocket.go
@@ -0,0 +1,12 @@
+package websocket
+
+import (
+	"github.com/stupboy/go-engine.io/transport"
+)
+
+var Creater = transport.Creater{
+	Name:      "websocket",
+	Upgrading: true,
+	Server:    NewServer,
+	Client:    NewClient,
+}
